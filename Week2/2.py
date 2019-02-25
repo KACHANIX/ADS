e3944d8bc380dd6af033bc095e47842dd5ea21b8
Sergei Kocharian <36566227+KACHANIX@users.noreply.github.com>
2019-02-25 03:29:17 +0300

    2-1 finished

--- Week2/2.py
+++ Week2/2.py
@@ -1,37 +1,36 @@
-def mergeSort(array, leftBorder, rightBorder, file):
+def mergeSort(array):
     if len(array) > 1:
         middle = len(array) // 2
         leftPart = array[:middle]
         rightPart = array[middle:]
-        mergeSort(leftPart, leftBorder, leftBorder + middle - 1, file)
-        mergeSort(rightPart, leftBorder + middle, rightBorder, file)
-
+        inversion = mergeSort(leftPart) + mergeSort(rightPart)
         leftIndex = 0
         rightIndex = 0
         position = 0
-        while (leftIndex < len(leftPart) and rightIndex < len(rightPart)):
-            if (leftPart[leftIndex] < rightPart[rightIndex]):
+        leftLen = len(leftPart)
+        rightLen = len(rightPart)
+        while leftIndex < leftLen and rightIndex < rightLen:
+            if leftPart[leftIndex] < rightPart[rightIndex]:
                 array[position] = leftPart[leftIndex]
                 leftIndex += 1
             else:
                 array[position] = rightPart[rightIndex]
                 rightIndex += 1
             position += 1
-        while (leftIndex < len(leftPart)):
+        while leftIndex < leftLen:
             array[position] = leftPart[leftIndex]
             position += 1
             leftIndex += 1
-        while (rightIndex < len(rightPart)):
+        while rightIndex < rightLen:
             array[position] = rightPart[rightIndex]
             position += 1
             rightIndex += 1
-        file.write(f"{leftBorder + 1} {rightBorder + 1} {array[0]} {array[-1]}\n")
-
+        return inversion
+    return 0
 
 
 file = open("input.txt", "r")
 quantity = int(file.readline())
 numbers = list(map(int, file.read().split()))
 file = open("output.txt", "w")
-mergeSort(numbers, 0, quantity - 1, file)
-file.write(' '.join(map(str, numbers)))
+file.write(f"{mergeSort(numbers)}")

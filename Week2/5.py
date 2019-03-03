9ab71074f4c447ba7671eaca3d7b188913173e09
user1 <user1@mail.ru>
2019-03-03 04:25:44 +0300

    2 week ready

--- Week2/5.py
+++ Week2/5.py
@@ -1,22 +1,20 @@
-def answer(array, quantity):
-    for i in range(1, quantity ):
-        if array[i] < array[i - 1]:
-            return "NO"
-    return "YES"
-
 file = open("input.txt", "r")
 firstLine = list(map(int, file.read().split()))
 quantity = firstLine[0]
 armSpan = firstLine[1]
 array = firstLine[2:]
-isRunning = True
-while(isRunning):
-    isRunning = False
-    for i in range(0, quantity - armSpan):
-        if array[i] > array[i + armSpan]:
-            array[i], array[i + armSpan] = array[i + armSpan], array[i]
-            isRunning = True
+
+arrays = []
+for i in range(armSpan):
+    tmpArr = []
+    for j in range (i, quantity, armSpan):
+        tmpArr.append(array[j])
+    tmpArr.sort()
+    arrays.append(tmpArr)
+result = "YES"
+for i in range(1, quantity):
+    if arrays[i % armSpan][i // armSpan] < arrays[(i - 1) % armSpan][(i - 1) // armSpan]:
+        result = "NO"
+        break
 file = open("output.txt", "w")
-
-file.write(answer(array, quantity))
-
+file.write(result)

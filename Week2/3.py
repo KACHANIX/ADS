11e47bdccf5e08dca1e4bc90cc5287ae5d5859c5
user1 <user1@mail.ru>
2019-03-03 04:02:59 +0300

    2-5 TL

--- Week2/3.py
+++ Week2/3.py
@@ -1,14 +1,7 @@
-def anti_quicksort(quantity):
-    indexes = [i for i in range(quantity)]
-    result = [None for i in range(quantity)]
-    for i in range(quantity - 1, -1, -1):
-        result[indexes[i // 2]] = i + 1
-        indexes[i // 2], indexes[i] = indexes[i], indexes[i // 2]
-    if quantity > 1:
-        result[result.index(1)], result[result.index(2)] = result[result.index(2)], result[result.index(1)]
-    output = (result)
-    return (' '.join(map(str, result)))
-
-
 quantity = int(open("input.txt", "r").readline())
-open("output.txt", "w").write(anti_quicksort(quantity))
+array = [0 for i in range(quantity)]
+for i in range(quantity):
+    array[i] = i + 1
+    if (i > 1):
+        array[i], array[i // 2] = array[i // 2], array[i]
+open("output.txt", "w").write(' '.join(map(str, array)))

87dd7aa3f992f432ef1719ce17ceb91dcfb8c73f
user1 <user1@mail.ru>
2019-03-19 01:42:39 +0300

    3 week ready

--- Week1/5.py
+++ Week1/5.py
@@ -2,7 +2,7 @@ file = open("input.txt", "r")
 quantity = int(file.readline())
 array = file.readline().split()
 file = open("output.txt", "w")
-
+str1 = ""
 for i in range(quantity - 1):
     minimum = int(array[i])
     minIndex = i
@@ -12,7 +12,8 @@ for i in range(quantity - 1):
             minimum = array[j]
     if minIndex != i:
         array[i], array[minIndex] = array[minIndex], array[i]
-        file.write(f'Swap elements at indices {i + 1} and {minIndex + 1}.\n')
+        str1 += (f'Swap elements at indices {i + 1} and {minIndex + 1}.\n')
 str2 = ' '.join(array)
+file.write(str1)
 file.write("No more swaps needed.\n")
 file.write(str2)
